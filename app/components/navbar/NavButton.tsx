e5cc242ecd48cfd3572f123ee27a3242d86961d5
ZL <ray040406@gmail.com>
2025-04-09 00:05:20 +1200

    flexibility change

--- app/components/navbar/NavButton.tsx
+++ app/components/navbar/NavButton.tsx
@@ -1,11 +1,18 @@
-import Link from "next/link";
-const NavButton = ({ link, name }: { link: string; name: string }) => {
+import Link, { type LinkProps } from "next/link";
+
+interface NavButtonProps extends LinkProps {
+  label: string;
+}
+
+const NavButton = ({ href, label, ...props }: NavButtonProps) => {
   return (
     // test NavButton component
-    <Link href={link}>
-      <div className="border-gray-700 flex-col border-2 text-center text-gray-600 rounded-md hover:bg-gray-400 hover:text-gray-200 duration-300 w-[300px] min-h-[40px] flex items-center justify-center">
-        <h1 className="text-2xl font-bold">{name}</h1>
-      </div>
+    <Link
+      {...props}
+      className="border-gray-700 flex-col border-2 text-center text-gray-600 rounded-md hover:bg-gray-400 hover:text-gray-200 duration-300 w-[300px] min-h-[40px] flex items-center justify-center"
+      href={href}
+    >
+      <h1 className="text-2xl font-bold">{label}</h1>
     </Link>
   );
 };
