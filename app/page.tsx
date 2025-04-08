c0791d4a7c88e8a32275547ba87fcbe629e2662a
ZL <ray040406@gmail.com>
2025-04-09 00:08:23 +1200

    update props

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="font-sans">
       Whereas disregard and contempt for
-      <NavButton link="google.com" name="TEST" />
+      <NavButton href="google.com" label="TEST" />
     </div>
   );
 }
