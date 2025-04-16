282d8cd26b92065e15e1c29a0e406b1529561fef
Ajith <ajith.penmatsa001@gmail.com>
2025-04-17 00:18:27 +1200

    added some text onto external links  page

--- app/external-resources/page.tsx
+++ app/external-resources/page.tsx
@@ -1,9 +1,20 @@
+import { ChevronDown } from 'lucide-react';
+
 const ExternalResources = () => {
   return (
-    <div>
-      <h1>NAVIGATING THROUGH EXTERNAL RESOURCES</h1>
-      <h2>insert description of the purpose of this page etc..</h2>
+    <div >
+      <div className="  text-center pt-30 space-y-2">
+        <h1 className="font-bold text-2xl">
+          NAVIGATING THROUGH <br /> EXTERNAL RESOURCES
+        </h1>
+        <h2>
+          insert description of the purpose <br /> of this page etc..
+        </h2>
+        <button className="border border-black bg-black text-white px-4 py-2 rounded opacity-80 hover:opacity-100 transition duration-300 ease-in-out">
+          Explore now <ChevronDown className="inline-block" />
+        </button>
+      </div>
     </div>
-  )
-}
-export default ExternalResources
+  );
+};
+export default ExternalResources;
