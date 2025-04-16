1194f3d57346e7136292f22ccd9ba284663a6351
Ajith <ajith.penmatsa001@gmail.com>
2025-04-16 21:49:34 +1200

    testing if prettier formats all the files

--- app/external-resources/page.tsx
+++ app/external-resources/page.tsx
@@ -1,4 +1,9 @@
 const ExternalResources = () => {
-  return <h1>External Resources</h1>
+  return (
+    <div>
+      <h1>NAVIGATING THROUGH EXTERNAL RESOURCES</h1>
+      <h2>insert description of the purpose of this page etc..</h2>
+    </div>
+  )
 }
 export default ExternalResources
