2da64bd7c26ca6db0de54f124dfd4ac35f6a9e81
zlrkw11 <64724157+zlrkw11@users.noreply.github.com>
2025-04-16 19:13:54 +1200

    chore: added nvmrc (#27)
    * chore: added nvmrc
    
    * chore: fix tsc error
    
    * chore: run prettier fix
    
    * chore: added back
    
    * chore: npm 10
    
    * chore: npx to all scripts
    
    * feat: fixed button props for navbar button
    
    * chore: installed nextjs version 15.3.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,5 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import '../styles/globals.css'
-
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap',
-  preload: true,
-})
 
 export const metadata: Metadata = {
   title: 'AUSA',
