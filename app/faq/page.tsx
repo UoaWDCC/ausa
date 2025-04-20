3672b7481acbf40297ed1d97a2b7877de2d5980b
Gavinzld <gavinzlding@gmail.com>
2025-04-20 23:19:50 +1200

    feat: added routing to faq and created template faq pag

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,4 +1,18 @@
+import Navbar from '../components/navbar/NavBar'
+
 const FAQ = () => {
-  return <h1>FAQ</h1>
+  return (
+    <div className="font-RG bg-white min-h-screen">
+      <Navbar />
+      <div className="w-full bg-gray-300 py-28 flex justify-center">
+        <h1 className="text-6xl text-black">FAQs</h1>
+      </div>
+      <div className="py-10 flex justify-center">
+        <p className="text-3xl font-RK text-black">
+          Anything you want to know from us?
+        </p>
+      </div>
+    </div>
+  )
 }
 export default FAQ
