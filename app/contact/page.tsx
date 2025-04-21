c6bc5b8e7d7fd3bd887e8578fbec00324ca61174
ganznz <loganbellinghamlb@gmail.com>
2025-04-21 14:51:32 +1200

    start implementation of ``/contacts`` page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,16 @@
+import Navbar from '../components/navbar/NavBar'
+import ContactForm from '../components/contact-form/ContactForm'
+
 const Contact = () => {
-  return <h1>Contact</h1>
+  return (
+    <div className="font-RG">
+      <Navbar /> {/* this is here temporarily */}
+      <div className="bg-white font-RK">
+        <div className="bg-[#D9D9D9]">
+          <ContactForm></ContactForm>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Contact
