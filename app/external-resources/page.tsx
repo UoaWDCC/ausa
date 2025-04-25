fca5db4bb1834fac4cafa9972f86e11492e61c9c
Daksh <75721773+Ajith05105@users.noreply.github.com>
2025-04-25 14:12:43 +1200

    changing the layout of the page

--- app/external-resources/page.tsx
+++ app/external-resources/page.tsx
@@ -1,22 +1,22 @@
 import { ChevronDown } from 'lucide-react'
-import ExternalLinkCard from '../components/ExternalLinkCard'
+import ExternalLinkCard from './ExternalLinkCard'
 import sampleImage from '../assets/images/sample.jpg'
 
 const ExternalResources = () => {
   return (
     <div>
-      <div className="space-y-2 flex flex-col ml-20 gap-4 p-2 mt-4">
+      <div className="space-y-2 flex flex-col ml-20 gap-4 p-2 justify-center h-screen ">
         <h1 className="font-bold text-2xl">
           NAVIGATING THROUGH <br /> EXTERNAL RESOURCES
         </h1>
-        <h2 className="mr-auto">
+        <h2>
           insert description of the purpose <br /> of this page etc..
         </h2>
         <button className="w-[200px] cursor-pointer border border-black bg-black text-white px-4 py-2 rounded opacity-80 hover:opacity-100 transition duration-300 ease-in-out">
           Explore now <ChevronDown className="inline-block" />
         </button>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10 gap-x-10 p-10 pl-20">
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10 gap-x-10 p-10 pl-20">
         <ExternalLinkCard
           ExternalLinkImage={sampleImage}
           title="card 1"
@@ -57,7 +57,7 @@ const ExternalResources = () => {
           title="card 8"
           description="some some some smoe"
         />
-      </div>
+      </div> */}
     </div>
   )
 }
