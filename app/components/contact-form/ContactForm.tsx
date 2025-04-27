30e13f15c1249d3d05341fddb37e7079e86ec6ba
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-04-27 12:33:16 +1200

    Add hover effects for form

--- app/components/contact-form/ContactForm.tsx
+++ app/components/contact-form/ContactForm.tsx
@@ -15,14 +15,14 @@ const ContactForm = () => {
     )
   }
   return (
-    <div className="bg-white rounded-[32px] p-5 text-black w-[300px] md:w-[700px] shadow-md drop-shadow-sm">
+    <div className="bg-white rounded-[32px] p-5 text-black w-[300px] md:w-[700px] shadow-md drop-shadow-sm duration-300 hover:scale-105">
       <form onSubmit={sendEmail} className="flex flex-col gap-4">
         <label className="flex flex-col font-bold">
           Full Name
           <input
             type="text"
             placeholder="Ray Zhao 🥵🥵🥵🥵😩"
-            className="bg-[#E2E2E2] placeholder-[#9D9D9D] rounded-lg p-2 mt-1"
+            className="bg-[#E2E2E2] placeholder-[#9D9D9D] rounded-lg p-2 mt-1 hover:outline hover:outline-black hover:outline-2"
           />
         </label>
         <label className="flex flex-col font-bold">
@@ -30,7 +30,7 @@ const ContactForm = () => {
           <input
             type="text"
             placeholder="example@mail.com"
-            className="bg-[#E2E2E2] placeholder-[#9D9D9D] rounded-lg p-2 mt-1"
+            className="bg-[#E2E2E2] placeholder-[#9D9D9D] rounded-lg p-2 mt-1 hover:outline hover:outline-black hover:outline-2"
           />
         </label>
         <label className="flex flex-col font-bold">
@@ -50,7 +50,7 @@ const ContactForm = () => {
           <textarea
             rows={6}
             placeholder="Write enquiry here"
-            className="bg-[#E2E2E2] placeholder-[#9D9D9D] rounded-lg p-2 mt-1"
+            className="bg-[#E2E2E2] placeholder-[#9D9D9D] rounded-lg p-2 mt-1 hover:outline hover:outline-black hover:outline-2"
           />
         </label>
         <button
