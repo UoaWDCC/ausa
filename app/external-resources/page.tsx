a3e5e3d0b83a40da82cf46f806ee1b55f2cb1a41
A.M. Zapata <61079957+fishchiatrist@users.noreply.github.com>
2025-04-29 02:57:35 +1200

    description is unnecessary (but can be added if not)

--- app/external-resources/page.tsx
+++ app/external-resources/page.tsx
@@ -21,9 +21,6 @@ const ExternalResources = () => {
         <h1 className="font-bold text-2xl">
           EXTERNAL RESOURCES
         </h1>
-        <h2>
-          insert description of the purpose <br /> of this page etc..
-        </h2>
         <button onClick = {() => {
           cardsRef.current?.scrollIntoView({behavior:'smooth'})
         }} className="w-[200px] cursor-pointer border border-black bg-black text-white px-4 py-2 rounded opacity-80 hover:opacity-100 transition duration-300 ease-in-out">
