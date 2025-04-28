15fb412a4d4306ed213914d849f76eebd2898076
zlrkw11 <ray040406@gmail.com>
2025-04-28 15:36:56 +1200

    refactor: next.config fix for svg file imports

---
+++ server/src/service-layer/controllers/UserController.ts
@@ -0,0 +1,21 @@
+// src/users/usersController.ts
+import { Body, Controller, Get, Path, Post, Query, Route, SuccessResponse } from 'tsoa'
+import { User } from 'data-layer/models/user'
+import UserDataService from 'data-layer/services/UserDataService'
+import { UserCreationParams } from 'service-layer/request-models/UserRequest'
+
+@Route('users')
+export class UsersController extends Controller {
+  @Get('{userId}')
+  public async getUser(@Path() userId: number, @Query() name?: string): Promise<User> {
+    return new UserDataService().get(userId, name)
+  }
+
+  @SuccessResponse('201', 'Created') // Custom success response
+  @Post()
+  public async createUser(@Body() requestBody: UserCreationParams): Promise<void> {
+    this.setStatus(201) // set return status 201
+    new UserDataService().create(requestBody)
+    return
+  }
+}
