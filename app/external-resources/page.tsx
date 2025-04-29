64cc4058bdeb51ab74a90452868072d307f84355
A.M. Zapata <61079957+fishchiatrist@users.noreply.github.com>
2025-04-29 14:32:46 +1200

    centered student wellfare category title

--- app/external-resources/page.tsx
+++ app/external-resources/page.tsx
@@ -40,7 +40,9 @@ const ExternalResources = () => {
           </button>
         </div>
 
-        <h1 className="font-bold text-2xl">Student Wellfare</h1>
+        <div className="flex justify-center">
+          <h1 className="font-bold text-2xl">Student Wellfare</h1>
+        </div>
         <div
           ref={wellRef}
           className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-10 gap-x-10 p-10 pl-20"
