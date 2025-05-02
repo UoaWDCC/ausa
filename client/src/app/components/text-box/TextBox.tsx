848c3aac09e0be8b6853bfb560aef61321b9a09e
ZL <ray040406@gmail.com>
2025-05-02 12:47:16 +1200

    feat: border property added for text box and set up display

--- client/src/app/components/text-box/TextBox.tsx
+++ client/src/app/components/text-box/TextBox.tsx
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 
 type TextBoxProps = {
   text: string
   theme: 'primary' | 'secondary' | 'ghost' | 'warning' | 'error'
   border?: boolean
+  children?: ReactNode
 }
 
-const themes: Record<TextBoxProps['theme'], { bg: string; text: string }> = {
-  primary: { bg: 'bg-primary-dark', text: 'text-white' },
-  secondary: { bg: 'bg-secondary', text: 'text-text-blue' },
-  ghost: { bg: '', text: 'text-ghost-dark' },
-  warning: { bg: '', text: 'text-warning' },
-  error: { bg: '', text: 'text-error' },
+const themes: Record<
+  TextBoxProps['theme'],
+  { bg: string; text: string; border: string }
+> = {
+  primary: {
+    bg: 'bg-primary-dark',
+    text: 'text-white',
+    border: 'border-secondary',
+  },
+  secondary: {
+    bg: 'bg-background',
+    text: 'text-text-blue',
+    border: 'border-secondary',
+  },
+  ghost: { bg: '', text: 'text-ghost-dark', border: 'border-ghost-light' },
+  warning: { bg: '', text: 'text-warning', border: 'border-warning' },
+  error: { bg: '', text: 'text-error', border: 'border-error' },
 }
 
-const TextBox = ({ text, theme, border }: TextBoxProps) => {
+const TextBox = ({ text, theme, border, children }: TextBoxProps) => {
   const styles = themes[theme]
   const className = React.useMemo(() => {
-    return `${styles.bg} ${styles.text} px-4 py-2 rounded-md flex flex-col justify-center w-[10rem] md:w-[20rem]`
+    return `${styles.bg} ${styles.text} ${border && `${styles.border} border-3`} px-4 py-2 rounded-md flex flex-col justify-center w-[10rem] md:w-[20rem]`
   }, [theme, border])
   return (
     <div className={className}>
       <p>{text}</p>
+      {children}
     </div>
   )
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,11 +2,61 @@ import TextBoxComponent from './components/text-box/TextBox'
 
 const Home = () => {
   return (
-    <div className="">
-      <TextBoxComponent
-        text="hello this is a primary textbox component"
-        theme="primary"
-      />
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-2">
+        <TextBoxComponent
+          text="hello this is a primary textbox component"
+          theme="primary"
+          border={true}
+        />
+        <TextBoxComponent
+          text="hello this is a secondary textbox component"
+          theme="secondary"
+          border={true}
+        />
+        <TextBoxComponent
+          text="hello this is a ghost textbox component"
+          theme="ghost"
+          border={true}
+        />
+        <TextBoxComponent
+          text="hello this is a warning textbox component"
+          theme="warning"
+          border={true}
+        />{' '}
+        <TextBoxComponent
+          text="hello this is a warning textbox component"
+          theme="error"
+          border={true}
+        />{' '}
+      </div>
+      <div className="flex gap-2">
+        <TextBoxComponent
+          text="hello this is a primary textbox component"
+          theme="primary"
+          border={false}
+        />
+        <TextBoxComponent
+          text="hello this is a secondary textbox component"
+          theme="secondary"
+          border={false}
+        />
+        <TextBoxComponent
+          text="hello this is a ghost textbox component"
+          theme="ghost"
+          border={false}
+        />
+        <TextBoxComponent
+          text="hello this is a warning textbox component"
+          theme="warning"
+          border={false}
+        />{' '}
+        <TextBoxComponent
+          text="hello this is a warning textbox component"
+          theme="error"
+          border={false}
+        />{' '}
+      </div>
     </div>
   )
 }
