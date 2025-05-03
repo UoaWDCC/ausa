09c2d6cdd37074d6da0340481b94792d86a37c0f
zlrkw11 <ray040406@gmail.com>
2025-05-03 19:47:24 +1200

    feat: all buttons styled and onClick function passed, added hover effects

--- client/src/app/components/button/Button.tsx
+++ client/src/app/components/button/Button.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
+'use client'
+import React, { FC } from 'react'
+import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from 'clsx'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 type ButtonProps = {
   theme:
     | 'primary'
     | 'secondary'
-    | 'danger'
     | 'ghost'
     | 'warning'
     | 'success'
     | 'error'
     | 'primary-out'
+    | 'secondary-out'
   disabled?: boolean
-  click?: void
+  click?: () => void
   border: boolean
 }
 
@@ -19,41 +27,59 @@ const themes: Record<
   { bg: string; text: string; border?: string }
 > = {
   primary: {
-    bg: 'bg-primary/20',
-    text: 'text-primary',
-    border: 'border-primary',
+    bg: 'bg-primary/50 hover:bg-primary/80',
+    text: 'text-primary-dark hover:text-white',
+    border: 'border-primary-dark/70',
   },
   secondary: {
-    bg: 'bg-secondary',
-    text: 'text-text-blue',
+    bg: 'bg-secondary/60 hover:bg-secondary/80',
+    text: 'text-accent hover:text-white',
+    border: 'border-secondary',
   },
-  danger: {
-    bg: 'bg-danger',
-    text: 'text-ghost-dark',
-    border: 'border-ghost-light',
+  warning: {
+    bg: 'bg-warning/10 hover:bg-warning/0',
+    text: 'text-warning',
+    border: 'border-warning hover:border-warning/50',
   },
   ghost: {
-    bg: 'bg-ghost',
+    bg: 'bg-ghost-dark/10 hover:bg-ghost-dark/0',
     text: 'text-ghost-dark',
-    border: 'border-ghost-light',
+    border: 'border-ghost-light hover:border-ghost-light/50',
   },
-  warning: { bg: '', text: 'text-warning', border: 'border-warning' },
   success: { bg: '', text: 'text-success', border: 'border-success' },
-  error: { bg: '', text: 'text-error', border: 'border-error' },
+  error: {
+    bg: 'bg-error/10 hover:bg-error/0',
+    text: 'text-error',
+    border: 'border-error hover:border-error/50',
+  },
   'primary-out': {
-    bg: '',
+    bg: 'bg-primary/10 hover:bg-primary/0',
     text: 'text-primary-dark',
-    border: 'border-primary-dark',
+    border: 'border-primary-dark hover:border-primary-dark/50',
+  },
+  'secondary-out': {
+    bg: 'bg-secondary/10 hover:bg-secondary/0',
+    text: 'text-primary',
+    border: 'border-primary hover:border-primary/50',
   },
 }
 
-const Button = ({ theme, border }: ButtonProps) => {
+const Button: FC<ButtonProps> = ({ theme, border, click }: ButtonProps) => {
   const styles = themes[theme]
   const className = React.useMemo(() => {
-    return `font-geist hover:${styles.bg}/50 px-4 py-2 rounded-md font-semibold cursor-pointer ${styles.bg} ${styles.text} ${border && `${styles.border} border-2`}`
+    return cn(
+      `font-geist px-4 py-2 rounded-md font-semibold duration-300 cursor-pointer ${styles.bg} ${styles.text} ${border && `${styles.border} border-2`}`,
+    )
   }, [theme, border])
   return (
-    <button className={className}>
+    <button
+      className={className}
+      onClick={() => {
+        if (typeof click === 'function') {
+          click()
+        }
+      }}
+    >
       <p>Button</p>
     </button>
   )
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,6 +4,13 @@ const Home = () => {
     <div className="m-4 flex gap-2">
       <ButtonComponent theme="primary" border={true} />
       <ButtonComponent theme="primary" border={false} />
+      <ButtonComponent theme="secondary" border={true} />
+      <ButtonComponent theme="secondary" border={false} />
+      <ButtonComponent theme="warning" border={true} />
+      <ButtonComponent theme="ghost" border={true} />
+      <ButtonComponent theme="error" border={true} />
+      <ButtonComponent theme="primary-out" border={true} />
+      <ButtonComponent theme="secondary-out" border={true} />
     </div>
   )
 }
