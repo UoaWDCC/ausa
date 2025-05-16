f4dedb2029e3fe082042a8cb10c8b827bbb2f037
ZL <ray040406@gmail.com>
2025-05-17 00:51:11 +1200

    feat: mt-4

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,7 +2,7 @@ import Card from './components/card/Card'
 // Removed unused imports and fixed module path if necessary
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center">
+    <div className="mt-4 flex w-screen flex-col items-center justify-center">
       <div className="grid w-[1000px] grid-cols-2">
         <div>
           <div className="grid grid-cols-1">
