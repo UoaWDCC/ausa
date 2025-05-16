f7f9500b09fbd69787ca1481d01440e6a60bbe9f
Ajith <ajith.penmatsa001@gmail.com>
2025-05-16 22:55:24 +1200

    added the tsx for card component from shadcn

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,15 +2,7 @@ import ButtonComponent from './components/button/Button'
 const Home = () => {
   return (
     <div className="m-4 flex gap-2">
-      <ButtonComponent theme="primary" border={true} />
-      <ButtonComponent theme="primary" border={false} />
-      <ButtonComponent theme="secondary" border={true} />
-      <ButtonComponent theme="secondary" border={false} />
-      <ButtonComponent theme="warning" border={true} />
-      <ButtonComponent theme="ghost" border={true} />
-      <ButtonComponent theme="error" border={true} />
-      <ButtonComponent theme="primary-out" border={true} />
-      <ButtonComponent theme="secondary-out" border={true} />
+
     </div>
   )
 }
