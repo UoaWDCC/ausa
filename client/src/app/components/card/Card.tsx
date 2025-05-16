276b113e6b2b74922fb8b96ca60ce853df88294d
ZL <ray040406@gmail.com>
2025-05-17 00:43:43 +1200

    feat: card centered

--- client/src/app/components/card/Card.tsx
+++ client/src/app/components/card/Card.tsx
@@ -5,7 +5,7 @@ type CardProps = {
 
 const Card = ({ title, content }: CardProps) => {
   return (
-    <div className="bg-primary font-geist flex flex-col rounded-md border p-2">
+    <div className="bg-primary font-geist m-2 flex flex-col rounded-md border p-2">
       <div className="">{title}</div>
       <div className="">{content}</div>
       <button className="rounded-md border bg-gray-700 text-white">more</button>
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,23 +2,25 @@ import Card from './components/card/Card'
 // Removed unused imports and fixed module path if necessary
 const Home = () => {
   return (
-    <div className="grid grid-cols-2">
-      <div>
-        <div className="grid grid-cols-1">
-          <Card title="11" content="hello" />
-        </div>{' '}
-        <div className="grid grid-cols-2">
-          <Card title="11" content="hello" />
-          <Card title="11" content="hello" />
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="grid w-[1000px] grid-cols-2">
+        <div>
+          <div className="grid grid-cols-1">
+            <Card title="11" content="hello" />
+          </div>{' '}
+          <div className="grid grid-cols-2">
+            <Card title="11" content="hello" />
+            <Card title="11" content="hello" />
+          </div>
         </div>
-      </div>
-      <div>
-        <div className="grid grid-cols-2">
-          <Card title="11" content="hello" />
-          <Card title="11" content="hello" />
-        </div>{' '}
-        <div className="grid grid-cols-1">
-          <Card title="11" content="hello" />
+        <div>
+          <div className="grid grid-cols-2">
+            <Card title="11" content="hello" />
+            <Card title="11" content="hello" />
+          </div>{' '}
+          <div className="grid grid-cols-1">
+            <Card title="11" content="hello" />
+          </div>
         </div>
       </div>
     </div>
