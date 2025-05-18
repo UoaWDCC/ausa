11fe9b8c27f45c0e99bca0766502ef6c96351ddd
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-05-18 19:51:09 +1200

    Create navLink.tsx

---
+++ client/src/app/components/navLink/navLink.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+interface NavLinkProps {
+  href: string
+  text: string
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ href, text }) => {
+  return (
+    <Link className="group transition-all duration-300 ease-in-out" href={href}>
+      <span className="bg-gradient-to-r from-gray-500 to-gray-500 bg-[length:0%_5px] bg-left-bottom bg-no-repeat pb-4 transition-all duration-500 ease-out group-hover:bg-[length:100%_5px]">
+        {text}
+      </span>
+    </Link>
+  )
+}
+
+export default NavLink
