caf672f2e6beec9ca7b13119b598e9d97e4905a1
midnightcuberx <drcubing27@gmail.com>
2025-05-20 16:53:45 +1200

    Add some sort of search functionality

--- client/src/app/components/navSearch/navSearch.tsx
+++ client/src/app/components/navSearch/navSearch.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
   Dialog,
   DialogContent,
@@ -10,8 +12,47 @@ import {
 import { Input } from '@/components/ui/input'
 import { MdOutlineSearch } from 'react-icons/md'
 import { NavSearchCard } from '../navSearchCard/navSearchCard'
+import React, { useState } from 'react'
+
+interface pageInfo {
+  title: string
+  description: string
+}
 
 const NavSearch: React.FC = () => {
+  const pages = [
+    {
+      title: 'Home',
+      description: 'Stuff about home',
+    },
+    {
+      title: 'FAQ',
+      description: 'Stuff about FAQ',
+    },
+    {
+      title: 'External Resources',
+      description: 'Stuff about External Resources',
+    },
+    {
+      title: 'Contact',
+      description: 'Stuff about Contact',
+    },
+  ]
+
+  const [searchValue, setSearchValue] = useState<string>('')
+  const [searchResults, setSearchResults] = useState<pageInfo[]>(pages)
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    setSearchValue(value)
+    const filteredResults = pages.filter(
+      (page) =>
+        page.title.toLowerCase().includes(value.toLowerCase()) ||
+        page.description.toLowerCase().includes(value.toLowerCase()),
+    )
+    setSearchResults(filteredResults)
+  }
+
   return (
     <div>
       <Dialog>
@@ -26,14 +67,26 @@ const NavSearch: React.FC = () => {
             <DialogDescription>
               <div className="flex h-6 items-center space-x-2">
                 <div className="grid flex-1 gap-2">
-                  <Input placeholder="Search" />
+                  <Input
+                    placeholder="Search"
+                    value={searchValue}
+                    onChange={handleSearch}
+                  />
                 </div>
                 <MdOutlineSearch className="size-2 h-6 w-6 cursor-pointer" />
               </div>
             </DialogDescription>
           </DialogHeader>
           <DialogFooter>
-            <NavSearchCard title="Test" description="Hi test" />
+            <div className="flex w-full flex-col gap-2">
+              {searchResults.map((res) => (
+                <NavSearchCard
+                  key={res.title}
+                  title={res.title}
+                  description={res.description}
+                />
+              ))}
+            </div>
           </DialogFooter>
         </DialogContent>
       </Dialog>
