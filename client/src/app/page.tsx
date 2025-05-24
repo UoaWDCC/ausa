71923d2e78a49d3ddbbcf156def782ca923adfbc
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2025-05-24 18:10:11 +1200

    feat: add carousel to homepage layout (not yet positioned)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,14 @@
 import { Button } from './components/button/Button'
 import Practice from './components/shapes/Parabola'
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from './components/carousel/Carousel'
+
 const Home = () => {
   return (
     <div>
@@ -10,7 +18,41 @@ const Home = () => {
       <Button variant="outline">Outline</Button>
       <Button variant="ghost">Ghost</Button>
       <Button variant="link">Link</Button> */}
-      <Practice />
+
+      {/* The parabola Background */}
+      <div>
+        <Practice />
+      </div>
+
+      <Carousel>
+        {/* Spaces next and previous button to top right */}
+        <div className="mb-4 flex justify-end space-x-2">
+          <CarouselPrevious className="static translate-x-0 translate-y-0 rotate-0" />
+          <CarouselNext className="static translate-x-0 translate-y-0 rotate-0" />
+        </div>
+
+        {/* Carousel content with responsive item sizes */}
+        <CarouselContent>
+          <CarouselItem className="basis-full sm:basis-1/2 md:basis-1/3">
+            <div className="h-64 bg-gray-200 pl-2 md:pl-4">Slide 1</div>
+          </CarouselItem>
+          <CarouselItem className="basis-full sm:basis-1/2 md:basis-1/3">
+            <div className="h-64 bg-gray-200 pl-2 md:pl-4">Slide 2</div>
+          </CarouselItem>
+          <CarouselItem className="basis-full sm:basis-1/2 md:basis-1/3">
+            <div className="h-64 bg-gray-200 pl-2 md:pl-4">Slide 3</div>
+          </CarouselItem>
+          <CarouselItem className="basis-full sm:basis-1/2 md:basis-1/3">
+            <div className="h-64 bg-gray-200 pl-2 md:pl-4">Slide 4</div>
+          </CarouselItem>
+          <CarouselItem className="basis-full sm:basis-1/2 md:basis-1/3">
+            <div className="h-64 bg-gray-200 pl-2 md:pl-4">Slide 5</div>
+          </CarouselItem>
+          <CarouselItem className="basis-full sm:basis-1/2 md:basis-1/3">
+            <div className="h-64 bg-gray-200 pl-2 md:pl-4">Slide 6</div>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
     </div>
   )
 }
