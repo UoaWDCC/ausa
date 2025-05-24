f44592ceae090d6d4d94a83329723a098a5af4b2
ZL <ray040406@gmail.com>
2025-05-24 17:15:51 +1200

    feat: added motion, added animation on the icon

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Image from 'next/image'
+import { motion } from 'motion/react'
 import { Button } from './components/button/Button'
 import Icon1 from './assets/icons/_.svg'
 
@@ -9,14 +11,29 @@ const Home = () => {
       <div className="flex h-[58vh] flex-row bg-[#FAF7F2]">
         {/* Left side text and button */}
         <div className="flex flex-col justify-center px-16">
-          <h1 className="font-geist mb-8 text-6xl font-semibold text-black">
+          <h1 className="font-geist mb-8 text-6xl font-semibold text-neutral-600">
             Your Support
           </h1>{' '}
-          <div className="ml-8 flex items-center justify-center gap-4 font-semibold">
+          <div className="items-center justify-center gap-8 font-semibold md:ml-8 md:flex">
             <div className="-translate-y-4">
-              <Image src={Icon1} width={80} height={80} alt="Purple Icon" />
-            </div>
-            <h1 className="font-geist mb-8 text-6xl text-black">Starts Here</h1>
+              <motion.div
+                animate={{
+                  scale: [1, 1.25, 1],
+                  rotate: [0, 180, -180, 0],
+                }}
+                transition={{
+                  duration: 16,
+                  repeat: Infinity,
+                  repeatType: 'mirror',
+                  ease: 'easeInOut',
+                }}
+              >
+                <Image src={Icon1} width={80} height={80} alt="Purple Icon" />
+              </motion.div>
+            </div>{' '}
+            <h1 className="font-geist mb-8 text-6xl text-neutral-600">
+              Starts Here.
+            </h1>
           </div>
           <p className="mb-6 text-2xl text-black">
             Insert description text about something
@@ -24,12 +41,13 @@ const Home = () => {
           <div>
             <Button
               variant="outline"
-              className="rounded-partial border-none bg-[#FEDEA5] px-6 py-6 text-black shadow-md hover:bg-[#FED280] hover:text-black"
+              className="rounded-partial cursor-pointer border-none bg-[#FEDEA5] px-6 py-6 text-black shadow-md hover:bg-[#FED280] hover:text-black"
             >
               Start Wellbeing Quiz
             </Button>
-          </div>
+          </div>{' '}
         </div>
+        {/* <div id="slider-component">slider</div> */}
       </div>
     </div>
   )
