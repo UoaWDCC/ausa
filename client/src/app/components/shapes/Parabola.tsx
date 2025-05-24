6de414105f23f009be689908f78eb02e5ce2915d
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2025-05-24 15:39:09 +1200

    feat: add Parabola component and integrate into Home page

---
+++ client/src/app/components/shapes/Parabola.tsx
@@ -0,0 +1,15 @@
+const Practice = () => {
+  return (
+    <div className="h-150 w-full overflow-hidden">
+      <svg
+        className="h-full w-full"
+        viewBox="0 0 1440 320"
+        preserveAspectRatio="none"
+      >
+        <path fill="#8A99EE" d="M0,160 Q720,-100 1440,160 L1440,320 L0,320 Z" />
+      </svg>
+    </div>
+  )
+}
+
+export default Practice
