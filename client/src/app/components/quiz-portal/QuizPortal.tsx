97f729d0706bd731ba503c74d29a930bbd99f12e
VINCENT <vwal290@aucklanduni.ac.nz>
2025-05-24 23:31:09 +1200

    improve responsiveness

--- client/src/app/components/quiz-portal/QuizPortal.tsx
+++ client/src/app/components/quiz-portal/QuizPortal.tsx
@@ -1,23 +1,27 @@
+import Link from 'next/link'
 import { Button } from '../button/Button'
 
 const QuizPortal = () => {
   return (
-    <div className="m-4 flex w-[450px] flex-col items-center rounded bg-gradient-to-b from-gray-300 via-gray-300 to-blue-200 p-4 text-black sm:m-6 sm:w-[600px] sm:p-6 md:m-10 md:w-[800px] md:p-8 lg:m-20 lg:w-[1200px] lg:p-10">
-      <h2 className="sm:text-md mb-2 md:text-lg lg:text-xl">
-        By answering a few questions
-      </h2>
-      <h1 className="mb-4 sm:text-xl md:text-2xl lg:text-3xl">
-        We Can Lead You to the Right Resources
-      </h1>
-      <p className="lg:text-md mb-6 sm:text-xs md:text-sm">
-        Information text / stats etc.
-      </p>
-      <div>
-        <Button variant="quiz" className="text-xs">
-          Start Wellbeing Quiz
-        </Button>
+    <div className="container mx-auto max-w-7xl px-8 sm:px-10 md:px-14 lg:px-20">
+      <div className="my-8 flex flex-col items-center rounded-md bg-gradient-to-b from-gray-300 via-gray-300 to-blue-200 p-6 sm:my-14 sm:py-8 md:my-18 md:py-10 lg:my-24 lg:py-12">
+        <h2 className="mb-2 max-w-prose text-center text-sm sm:text-base md:text-lg">
+          By answering a few questions
+        </h2>
+        <h1 className="mb-4 max-w-prose text-center text-lg sm:text-xl md:text-2xl lg:text-3xl">
+          We Can Lead You to the Right Resources
+        </h1>
+        <p className="mb-6 max-w-prose text-center text-xs sm:text-sm md:text-base">
+          Information text / stats etc.
+        </p>
+        <div>
+          <Button variant="quiz" className="text-xs">
+            <Link href="/quiz">Start Wellbeing Quiz</Link>
+          </Button>
+        </div>
       </div>
     </div>
   )
 }
+
 export default QuizPortal
