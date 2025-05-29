63765d5ea5c32933e3bf02e2a199d15bafd9b6db
zlrkw11 <64724157+zlrkw11@users.noreply.github.com>
2025-05-29 20:47:18 +1200

    74 implement footer component (#85)
    * feat: entire footer template
    
    * feat: fixed footer responsiveness
    
    * feat: fixed responsiveness bottom row
    
    * added css styling for headings in footer
    
    * added 1 ausa logo (black and white version)
    
    * added 4 svg icons for footer links: facebook, instagram, linkedin
    
    * imported logo + 3 svg icons, made sure footer is responsive to small screens too
    
    * feat: fixed footer at bottom
    
    * style: color to white from black for social media icons
    
    * style: restyled the layout on mobile and desktop versions
    
    * feat: added margin for social media icons
    
    * feat: links + attempt perfect spacing first row
    
    * feat: fixed bugs
    
    * feat: added back my CODE
    
    * feat: changed icons to links
    
    * feat: fixed small viewport layout
    
    * style: prettier
    
    * error: lint
    
    ---------
    
    Co-authored-by: ashoomky <ashlee@shum.co.nz>
    Co-authored-by: A.M. Zapata <61079957+fishchiatrist@users.noreply.github.com>

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,4 @@
-import { Button } from './components/button/Button'
-
 const Home = () => {
-  return (
-    <div className="m-4 flex gap-2">
-      <Button>Button</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="link">Link</Button>
-    </div>
-  )
+  return <div className="m-4 flex gap-2"></div>
 }
 export default Home
