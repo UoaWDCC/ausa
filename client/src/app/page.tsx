d33982b9c73939e2083702745656c72e401de00c
ZL <ray040406@gmail.com>
2025-06-01 12:55:16 +1200

    refactor: adjusted positioning for the component on top of the footer

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,3 @@
-
 // import { Button } from './components/button/Button'
 
 import {
@@ -12,54 +11,33 @@ import Practice from './components/shapes/Parabola'
 
 const Home = () => {
   return (
-    <div className="relative overflow-hidden">
-
+    <div className="">
       {/* The parabola Background */}
-      <div className="">
-        <div className="absolute inset-0 z-0 bottom-0">
+      <div className="translate-y-[-0px]">
+        <div className="absolute inset-0 bottom-0 z-0">
           <Practice />
         </div>
-        
+
         <div className="relative z-10 px-4 pt-100">
-        <Carousel>
-          {/* Spaces next and previous button to top right */}
-          <div className="mb-4 flex justify-end space-x-2">
-            <CarouselPrevious className="static translate-x-0 translate-y-0 rotate-0" />
-            <CarouselNext className="static translate-x-0 translate-y-0 rotate-0" />
-          </div>
+          <Carousel>
+            {/* Spaces next and previous button to top right */}
+            <div className="mb-4 flex justify-end space-x-2">
+              <CarouselPrevious className="static translate-x-0 translate-y-0 rotate-0" />
+              <CarouselNext className="static translate-x-0 translate-y-0 rotate-0" />
+            </div>
 
-          {/* Carousel content with responsive item sizes */}
-          <CarouselContent className="mb-10 -ml-2 md:-ml-4">
-
-            <CarouselItem>
-              slide #1
-            </CarouselItem>
-
-            <CarouselItem>
-              slide #2
-            </CarouselItem>
-
-            <CarouselItem>
-              slide #3
-            </CarouselItem>
-
-            <CarouselItem>
-              slide #4
-            </CarouselItem>
-
-            <CarouselItem>
-              slide #5
-            </CarouselItem>
-
-            <CarouselItem>
-              slide #6
-            </CarouselItem>
-
-          </CarouselContent>
-        </Carousel>
+            {/* Carousel content with responsive item sizes */}
+            <CarouselContent className="mb-10 -ml-2 md:-ml-4">
+              <CarouselItem>slide #1</CarouselItem>
+              <CarouselItem>slide #2</CarouselItem>
+              <CarouselItem>slide #3</CarouselItem>
+              <CarouselItem>slide #4</CarouselItem>
+              <CarouselItem>slide #5</CarouselItem>
+              <CarouselItem>slide #6</CarouselItem>
+            </CarouselContent>
+          </Carousel>
+        </div>
       </div>
-      </div>
-
     </div>
   )
 }
