25ca2df0b8ec95afaa8befbde4fafc8f501ab559
ZL <ray040406@gmail.com>
2025-06-02 19:23:07 +1200

    feat: clear out home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,8 @@
-import { Button } from './components/button/Button'
 import QuizPortal from './components/quiz-portal/QuizPortal'
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="m-4 flex gap-2">
-        <Button>Button</Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="outline">Outline</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="link">Link</Button>
-      </div>
-
+    <div className="flex h-screen flex-col items-center justify-center">
       <QuizPortal />
     </div>
   )
