9630bb4199b0eb78765d432768f8614c8fa7c011
Tulza <Tulza0410@gmail.com>
2025-06-08 15:06:18 +1200

    feat: fly and docker config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  webpack: (config) => {
+    config.resolve = config.resolve || {}
+    config.resolve.alias = config.resolve.alias || {}
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  },
   /* config options here */
 }
 
