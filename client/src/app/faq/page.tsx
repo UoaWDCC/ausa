216cd61c61be1f9251bf4cface78da8588d97c6c
zlrkw11 <ray040406@gmail.com>
2025-06-09 17:29:33 +1200

    feat: added room for the contents to not be covered

--- client/src/app/faq/page.tsx
+++ client/src/app/faq/page.tsx
@@ -1,13 +1,10 @@
-import NavigationBar from '../components/navbar/Navbar_final'
-
 const Banner = () => {
   return <div className="border border-yellow-300 p-4"></div>
 }
 
 const FAQ = () => {
   return (
-    <div className="flex min-h-screen flex-col">
-      <NavigationBar />
+    <div className="mt-[70px] flex min-h-screen flex-col">
       <div className="grid h-[200px] w-full grid-cols-5">
         {' '}
         <div className="col-span-1 border" id="section-left">
