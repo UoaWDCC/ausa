d785ffcc9f739f5289a10fc272b5a88d6496ba56
ZL <ray040406@gmail.com>
2025-06-09 15:07:55 +1200

    feat: structure for the page (content covered by navbar)

--- client/src/app/faq/page.tsx
+++ client/src/app/faq/page.tsx
@@ -1,4 +1,25 @@
-const FAQ = () => {
-  return <h1>FAQ</h1>
+import NavigationBar from '../components/navbar/Navbar_final'
+
+const Banner = () => {
+  return <div className="border border-yellow-300 p-4"></div>
 }
+
+const FAQ = () => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <NavigationBar />
+      <div className="grid h-[200px] w-full grid-cols-5">
+        {' '}
+        <div className="col-span-1 border" id="section-left">
+          1
+        </div>
+        <div className="col-span-4 border" id="section-right">
+          4
+        </div>
+      </div>
+      <Banner />
+    </div>
+  )
+}
+
 export default FAQ
