050883af7a8baac58265ddb1e04c10d884369a5a
ZL <ray040406@gmail.com>
2025-06-09 22:44:51 +1200

    feat: mobile first layout using grid

--- client/src/app/faq/page.tsx
+++ client/src/app/faq/page.tsx
@@ -5,38 +5,40 @@ const Banner = () => {
 const FAQ = () => {
   return (
     <div className="mt-[70px] flex flex-col">
-      <div className="grid h-[600px] w-full grid-cols-5">
+      <div className="font-geist flex h-[600px] w-full flex-col md:grid md:grid-cols-3 lg:grid-cols-4">
         <div
-          className="col-span-1 m-4 flex flex-col gap-4 border"
+          className="col-span-1 m-8 flex flex-col justify-center gap-4 bg-[#5A9B8C] text-white"
           id="section-left"
         >
-          <h1 className="flex justify-center">How Can We Help?</h1>
-          <div className="flex flex-col gap-2 border">
-            <h2>University Support</h2>
+          <h1 className="flex justify-center text-xl font-bold">
+            How Can We Help?
+          </h1>
+          <div className="flex flex-col items-center gap-2">
+            <h2 className="flex text-lg font-semibold">University Support</h2>
             <ul className="ml-4 list-disc">
               <li>Campus Care</li>
               <li>General Wellbeing Support</li>
               <li>Privacy Concerns</li>
             </ul>
           </div>
-          <div className="border">
-            <h2>External Supoort</h2>
+          <div className="flex flex-col items-center gap-2">
+            <h2 className="text-lg font-semibold">External Supoort</h2>
             <ul className="ml-4 list-disc">
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>Campus Care</li>
+              <li>General Wellbeing Support</li>
+              <li>Privacy Concerns</li>
             </ul>
           </div>
-          <div className="border">
-            <h2>Emergency Support</h2>
+          <div className="flex flex-col items-center gap-2">
+            <h2 className="text-lg font-semibold">Emergency Support</h2>
             <ul className="ml-4 list-disc">
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>Campus Care</li>
+              <li>General Wellbeing Support</li>
+              <li>Privacy Concerns</li>
             </ul>
           </div>
         </div>
-        <div className="col-span-4 border" id="section-right">
+        <div className="border md:col-span-2 lg:col-span-3" id="section-right">
           4
         </div>
       </div>
