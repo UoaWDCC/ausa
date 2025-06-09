4792804803e3838c186cdfb996672b106048dca9
ZL <ray040406@gmail.com>
2025-06-09 22:47:46 +1200

    feat: adjust spacing, left section complete

--- client/src/app/faq/page.tsx
+++ client/src/app/faq/page.tsx
@@ -7,13 +7,13 @@ const FAQ = () => {
     <div className="mt-[70px] flex flex-col">
       <div className="font-geist flex h-[600px] w-full flex-col md:grid md:grid-cols-3 lg:grid-cols-4">
         <div
-          className="col-span-1 m-8 flex flex-col justify-center gap-4 bg-[#5A9B8C] text-white"
+          className="col-span-1 flex flex-col justify-center gap-4 bg-[#5A9B8C] p-8 text-white"
           id="section-left"
         >
-          <h1 className="flex justify-center text-xl font-bold">
+          <h1 className="mb-2 flex justify-center text-2xl font-bold">
             How Can We Help?
           </h1>
-          <div className="flex flex-col items-center gap-2">
+          <div className="mb-4 flex flex-col items-center gap-2">
             <h2 className="flex text-lg font-semibold">University Support</h2>
             <ul className="ml-4 list-disc">
               <li>Campus Care</li>
@@ -21,7 +21,7 @@ const FAQ = () => {
               <li>Privacy Concerns</li>
             </ul>
           </div>
-          <div className="flex flex-col items-center gap-2">
+          <div className="mb-4 flex flex-col items-center gap-2">
             <h2 className="text-lg font-semibold">External Supoort</h2>
             <ul className="ml-4 list-disc">
               <li>Campus Care</li>
@@ -29,7 +29,7 @@ const FAQ = () => {
               <li>Privacy Concerns</li>
             </ul>
           </div>
-          <div className="flex flex-col items-center gap-2">
+          <div className="mb-4 flex flex-col items-center gap-2">
             <h2 className="text-lg font-semibold">Emergency Support</h2>
             <ul className="ml-4 list-disc">
               <li>Campus Care</li>
@@ -38,9 +38,10 @@ const FAQ = () => {
             </ul>
           </div>
         </div>
-        <div className="border md:col-span-2 lg:col-span-3" id="section-right">
-          4
-        </div>
+        <div
+          className="border md:col-span-2 lg:col-span-3"
+          id="section-right"
+        ></div>
       </div>
       <Banner />
     </div>
