2d060d5bbbd476e6b107cec11cb23d1ba77dab70
ZL <ray040406@gmail.com>
2025-06-24 21:39:42 +1200

    feat: portal construct and styling

--- client/src/app/portal/page.tsx
+++ client/src/app/portal/page.tsx
@@ -1,14 +1,21 @@
 import Link from 'next/link'
 const Portal = () => {
   return (
-    <div className="flex flex-col items-center">
-      Portal
-      <Link href="/quiz">
-        <button>quiz</button>
-      </Link>
-      <Link href="/faq">
-        <button>faq</button>
-      </Link>
+    <div className="mt-[70px] flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">Portal</h1>
+      <div className="flex gap-16">
+        {' '}
+        <Link href="/quiz">
+          <button className="cursor-pointer rounded-md border bg-gray-200 px-8 py-4 text-2xl hover:bg-white">
+            quiz
+          </button>
+        </Link>
+        <Link href="/faq">
+          <button className="cursor-pointer rounded-md border bg-gray-200 px-8 py-4 text-2xl hover:bg-white">
+            faq
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
