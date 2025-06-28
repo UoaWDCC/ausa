1c63ad84dde85f5a625dcdebf07b3d8fa795d5df
zlrkw11 <ray040406@gmail.com>
2025-06-28 16:56:43 +1200

    feat: reconstruct portal sections

--- client/src/app/portal/page.tsx
+++ client/src/app/portal/page.tsx
@@ -3,20 +3,22 @@ const Portal = () => {
   return (
     <div className="font-geist mt-[70px] flex h-screen flex-col items-center justify-center gap-4 bg-[#393980]">
       <div className="flex flex-col items-center gap-4 rounded-md border-4 border-gray-500 bg-white p-8">
-        <h1 className="text-3xl font-bold">Portal</h1>
-        <div className="flex gap-16">
+        <h1 className="border text-xl font-bold">portal component</h1>
+        <div className="flex gap-4">
           {' '}
           <Link href="/quiz">
-            <button className="cursor-pointer rounded-md border bg-gray-400 px-8 py-4 text-2xl hover:bg-white">
+            <button className="cursor-pointer border bg-gray-400 px-8 py-4 text-2xl hover:bg-white">
               quiz
             </button>
           </Link>
           <Link href="/faq">
-            <button className="cursor-pointer rounded-md border bg-gray-400 px-8 py-4 text-2xl hover:bg-white">
+            <button className="cursor-pointer border bg-gray-400 px-8 py-4 text-2xl hover:bg-white">
               faq
             </button>
           </Link>
         </div>
+        <div className="border p-2">sign up</div>
+        <div className="border p-2">log in</div>
       </div>
     </div>
   )
