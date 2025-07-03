aa027d30a7950f13ed50692267108118b3d0d656
ZL <ray040406@gmail.com>
2025-07-03 23:04:28 +1200

    feat: increase gap

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ import linkedin from '../../app/assets/icons/linkedin.svg'
 
 const Footer = () => {
   return (
-    <div className="bottom-0 left-0 z-50 w-full transition-all duration-500 ease-out p-4 text-white bg-slate-900/95 backdrop-blur-md grid grid-cols-3">
+    <div className="bottom-0 left-0 z-50 w-full transition-all duration-500 ease-out p-6 text-white bg-slate-900/95 backdrop-blur-md grid grid-cols-3">
       {/* col1 */}
       <div className="flex flex-col ">
         <div className="flex flex-col gap-4 p-2">
