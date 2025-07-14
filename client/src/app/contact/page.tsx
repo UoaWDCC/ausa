01427d25b9686211cfa3f3000562c7d33a1e80f6
nicky8566 <nickytian2005@hotmail.com>
2025-07-14 23:04:29 +1200

    style: removed unecessary text and added ausa bg

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -6,20 +6,23 @@ import Image from 'next/image'
 import facebook from '../../app/assets/icons/facebook.svg'
 import instagram from '../../app/assets/icons/instagram.svg'
 import linkedin from '../../app/assets/icons/linkedin.svg'
+import { TiledAusaBackground } from '@/components/ausa/TiledAusaBackground'
 
 const Contact = () => {
   return (
     <div className="font-RG pt-20">
       <div className="font-RK">
         <div className="min-h-screen flex flex-col items-center overflow-hidden mb-0 pb-0">
-          <div className="mt-2 flex w-[90%] flex-col items-center gap-10 rounded-[55px] bg-[#2C2C2C] p-8 md:w-[95%] md:justify-center lg:max-w-[1500px] xl:max-w-[1800px] xl:flex-row xl:p-10">
-            <div className="flex flex-col items-center gap-5 text-center xl:items-start xl:gap-25 xl:text-left">
-              <div>
-                {/* only show this text on wide-screen displays */}
-                <p className="mb-2 hidden text-xl text-white xl:block">
-                  Submit an enquiry we’re here to help
-                </p>
+          <div className="relative mt-2 flex w-[90%] flex-col items-center gap-10 rounded-[55px] bg-[#2C2C2C] p-8 md:w-[95%] md:justify-center lg:max-w-[1500px] xl:max-w-[1800px] xl:flex-row xl:p-10 overflow-hidden">
 
+            {/* Background inside black box */}
+            <div className="absolute inset-0 z-0 rounded-[55px] overflow-hidden">
+              <TiledAusaBackground />
+            </div>
+
+            {/* Content above background */}
+            <div className="relative z-10 flex flex-col items-center gap-5 text-center xl:items-start xl:gap-25 xl:text-left">
+              <div>
                 {/* only show this text on wide-screen displays */}
                 <h1 className="hidden text-3xl font-bold text-white xl:block xl:text-5xl">
                   Let’s Talk
@@ -56,7 +59,7 @@ const Contact = () => {
               </div>
             </div>
 
-            <div>
+            <div className="relative z-10">
               <ContactForm />
             </div>
           </div>
