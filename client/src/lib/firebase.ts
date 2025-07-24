cf12a1da77df290b976055f5b6a7813ff4b0a623
zlrkw11 <ray040406@gmail.com>
2025-07-24 16:15:25 +1200

    feat: change

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -1,7 +1,8 @@
 "use client"
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, Auth } from "firebase/auth";
+import { Firestore, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth: Auth = getAuth(app);
+export const db: Firestore = getFirestore(app);
 export default app;
