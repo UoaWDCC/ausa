b96a58c8b43d973bab60bb217791bdfa74b45a6f
Ashlee Shum <ashu443@aucklanduni.ac.nz>
2025-08-04 20:54:01 +1200

    feat: added profile image upload

--- client/src/app/me/page.tsx
+++ client/src/app/me/page.tsx
@@ -1,38 +1,12 @@
-import Sidebar from '@/components/sidebar/sidebar'
+'use client'
+ import Sidebar from '@/components/sidebar/sidebar'
 import { IoMdPerson } from "react-icons/io";
 import { FaBell } from "react-icons/fa";
 import { MdLock } from "react-icons/md";
 import { TbWorld } from "react-icons/tb";
 import { LuMessageCircleQuestion } from "react-icons/lu";
 
-export default async function Me() {
-  /*return (
-    <div className="w-full min-h-[100vh] flex flex-col items-center py-30 gap-4">
-      <ProfileInfo />
-      <EventsList
-        events={[
-          {
-            id: '1',
-            title: 'Event 1',
-            date: '2023-10-01',
-            location: 'New York, NY',
-          },
-          {
-            id: '2',
-            title: 'Event 2',
-            date: '2023-10-05',
-            location: 'Los Angeles, CA',
-          },
-          {
-            id: '3',
-            title: 'Event 3',
-            date: '2023-10-10',
-            location: 'Chicago, IL',
-          },
-        ]}
-      />
-    </div>
-  )*/
+export default function Me() {
   const links = [
     { label: 'Profile', href: '/me/profile', icon: IoMdPerson },
     { label: 'Notifications', href: '/me/notifications', icon: FaBell },
@@ -40,9 +14,36 @@ export default async function Me() {
     { label: 'Language', href: '/me/languages', icon: TbWorld },
     { label: 'Help', href: '/me/events', icon: LuMessageCircleQuestion},
   ];
+
+  const handleImageUpload = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'image/*';
+    input.click();
+  };
+
   return (
-    <div className="w-full min-h-[100vh] h-full">
+    <div className="w-full min-h-[100vh] h-full flex">
       <Sidebar links={links} />
+      <div className="flex-1 px-8 pt-40 bg-[#FAF7F2]">
+        <h1 className="text-4xl font-semibold text-black mb-4">Account Settings</h1>
+        {/* black line */}
+        <div className="w-full h-px bg-black mb-10"></div>
+        {/* profile circle container */}
+        <div className="flex flex-col items-start gap-6 mb-8">
+          {/* profile icon */}
+          <div className="w-70 h-70 bg-gray-300 rounded-full flex items-center justify-center">
+            <IoMdPerson className="text-4xl text-gray-600" />
+          </div>
+          <button 
+            onClick={handleImageUpload}
+            className="pl-19 text-gray-800 hover:text-blue-400 underline cursor-pointer"
+          >
+            Upload an image
+          </button>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
+
