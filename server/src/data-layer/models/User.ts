e1f0e8bbd38f1e940d9df511794595c48fb0145a
ZL <ray040406@gmail.com>
2025-08-08 12:47:07 +1200

    feat: admin schema

--- server/src/data-layer/models/User.ts
+++ server/src/data-layer/models/User.ts
@@ -1,12 +1,18 @@
-export type User = {
+export interface User {
   id: string
   username: string
   email: string
   name: string
 }
 
+// for patch
 export interface UpdateUserPackage {
   username?: string
   name?: string
   email?: string
+}
+
+export interface admin extends User {
+  role: 'admin'
+  permissions: string[]
 }
