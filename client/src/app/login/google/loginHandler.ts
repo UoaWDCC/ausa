494efb01d24a89048b1d3a7ff6bb7ab9ff6babc7
zlrkw11 <ray040406@gmail.com>
2025-08-11 14:52:12 +1200

    feat: changed back to login with popup

--- client/src/app/login/google/loginHandler.ts
+++ client/src/app/login/google/loginHandler.ts
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { auth } from '@/lib/firebase'
 
 const handleGoogleLogin = async () => {
@@ -6,10 +6,12 @@ const handleGoogleLogin = async () => {
     const provider = new GoogleAuthProvider()
     provider.addScope('profile')
     provider.addScope('email')
-    await signInWithRedirect(auth, provider)
+    await signInWithPopup(auth, provider)
+    alert("Login Successful!")
   } catch (err: any) {
     console.error('Google login error:', err.code, err.message)
     alert('Failed to login with Google. Please try again.')
   }
 }
+
 export { handleGoogleLogin }
