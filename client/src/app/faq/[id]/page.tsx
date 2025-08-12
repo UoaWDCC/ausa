c6f137839ad0d1bebded1481ae2a150f9c8e4081
Dennis Hu <63829007+midnightcuberx@users.noreply.github.com>
2025-08-13 00:24:11 +1200

    update faq page to use tsoa fetch

--- client/src/app/faq/[id]/page.tsx
+++ client/src/app/faq/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { redirect } from 'next/navigation'
+import client from '@/services/fetch-client'
 import {
   Accordion,
   AccordionContent,
@@ -5,7 +7,6 @@ import {
   AccordionTrigger,
 } from '@/shadcn_components/ui/accordion'
 import type { Faq, FaqCategory } from '@/types/types'
-import { redirect } from 'next/navigation'
 import FaqBox from '../../../components/faq-box/FaqBox'
 import ausa from '../../assets/icons/ausa.svg'
 
@@ -43,51 +44,38 @@ export default async function FAQ({ params }: FAQProps) {
   const faqCategoryMap: Record<string, Faq[]> = {}
 
   const { id } = await params
-  /*if (!title[id]) {
-    redirect('/404')
-  }*/
-  const url = process.env.BACKEND_URL || 'http://localhost:8000'
-  const res = await fetch(`${url}/faq-category?url=${id}`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    method: 'GET',
-    mode: 'cors',
+  const res = await client.GET('/faq-category', {
+    params: { query: { url: id } },
   })
-  const faqCategory = await res.json()
 
-  if (res.status !== 200 || faqCategory.data.length === 0) {
+  console.log(res)
+
+  if (res.response.status !== 200 || res.data?.data?.length === 0) {
     redirect('/404')
   }
+  const faqCategory = res.data?.data || []
 
-  const res2 = await fetch(`${url}/faq-category`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    method: 'GET',
-    mode: 'cors',
-  })
-  const faqCategories = await res2.json()
-  if (res2.status !== 200) {
+  const res2 = await client.GET('/faq-category')
+  if (res2.response.status !== 200) {
     redirect('/500') //redirect to error page?
   }
+  const faqCategories = res2.data?.data || []
 
   await Promise.all(
-    faqCategories.data.map(async (category: FaqCategory) => {
-      const res = await fetch(`${url}/faq?category=${category.id}`, {
-        headers: { 'Content-Type': 'application/json' },
-        method: 'GET',
-        mode: 'cors',
+    faqCategories.map(async (category: FaqCategory) => {
+      const res = await client.GET('/faq', {
+        params: { query: { category: category.id } },
       })
-      const faqData = await res.json()
-      faqCategoryMap[category.id] = faqData.data.sort((a: Faq, b: Faq) =>
+      console.log('res', res.response.url)
+      const faqData = res.data?.data || []
+      faqCategoryMap[category.id] = faqData.sort((a: Faq, b: Faq) =>
         a.question.localeCompare(b.question),
       )
     }),
   )
 
   //console.log("faqCategory", faqCategories)
-  //console.log('faqCategoryMap', faqCategoryMap)
+  console.log('faqCategoryMap', faqCategoryMap)
 
   return (
     <div className="mt-[70px] flex min-h-[100vh] flex-col">
@@ -100,19 +88,19 @@ export default async function FAQ({ params }: FAQProps) {
             How Can We Help?
           </h1>
           <FaqBox
-            icon={ausa}
-            title={faqCategories.data[0].name}
             content="blahblahblah"
+            icon={ausa}
+            title={faqCategories[0].name}
           />
           <FaqBox
-            icon={ausa}
-            title={faqCategories.data[1].name}
             content="blahblahblah"
+            icon={ausa}
+            title={faqCategories[1].name}
           />
           <FaqBox
-            icon={ausa}
-            title={faqCategories.data[2].name}
             content="blahblahblah"
+            icon={ausa}
+            title={faqCategories[2].name}
           />
           {/* {faqCategories.data.map((category: FaqCategory) => (
              <div
@@ -134,11 +122,11 @@ export default async function FAQ({ params }: FAQProps) {
           id="section-right"
         >
           <h1 className="text-2xl md:text-4xl">Frequently Asked Questions</h1>
-          <h2 className="text-2xl font-semibold">{faqCategory.data[0].name}</h2>
+          <h2 className="text-2xl font-semibold">{faqCategory[0]?.name}</h2>
           <div className="">
-            <Accordion type="single" collapsible className="w-full">
-              {faqCategoryMap[faqCategory.data[0].id]?.map((faq: Faq) => (
-                <AccordionItem value={`item-${faq.id}`} key={faq.id}>
+            <Accordion className="w-full" collapsible type="single">
+              {faqCategoryMap[faqCategory[0]?.id]?.map((faq: Faq) => (
+                <AccordionItem key={faq.id} value={`item-${faq.id}`}>
                   <AccordionTrigger>{faq.question}</AccordionTrigger>
                   <AccordionContent>{faq.answer}</AccordionContent>
                 </AccordionItem>
