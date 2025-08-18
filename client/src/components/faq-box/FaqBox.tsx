2b1094b2009c8ea86c98751f55d7fbf8a18f1378
Ashlee Shum <ashu443@aucklanduni.ac.nz>
2025-08-18 12:19:01 +1200

    feat: changes to faq frot end part 2

--- client/src/components/faq-box/FaqBox.tsx
+++ client/src/components/faq-box/FaqBox.tsx
@@ -8,13 +8,13 @@ type FaqBoxProps = {
 
 const FaqBox = ({ icon, content, title }: FaqBoxProps) => {
   return (
-    <div className="flex gap-4 w-[250px] border rounded-md p-2 bg-slate-500/70 hover:bg-slate-500/80 transition-all duration-300 font-ReemKufi">
-      <div>
-        <Image alt="" height={20} src={icon} width={20} />
+    <div className="w-full flex flex-col sm:flex-row gap-4 max-w-[300px] sm:max-w-[280px] md:max-w-[350px]  border-neutral-300 border-3 rounded-md p-2 m-2 bg-neutral-100 hover:bg-white transition-all duration-300">
+      <div className = "flex-shrink-0 flex items-center">
+        <Image alt="" height={40} src={icon} width={40} />
       </div>
-      <div className="text-slate-200">
-        <h1 className="text-lg ">{title}</h1>
-        <p className="text-slate-400">{content}</p>
+      <div className="text-black">
+        <h1 className="text-lg">{title}</h1>
+        <p className="text-[#2D3B4E]">{content}</p>
       </div>
     </div>
   )
