7f647ad2dea5a7d83fa0474705f3545ba44b432c
A.M. Zapata <annameepymoop@gmail.com>
2025-08-31 20:22:30 +1200

    front end for quiz results tab on profile page

--- client/src/components/profile/quiz-results.tsx
+++ client/src/components/profile/quiz-results.tsx
@@ -1,4 +1,32 @@
+import Link from 'next/link'
+
 const ProfileQuizResults = () => {
-  return <div>Quiz Results Section</div>
+  return (
+    <div>
+      <div className="py-5">
+        <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+          Quiz Results
+        </h2>
+        <p className="text-gray-600">
+          No quiz results available at the moment.
+          <Link
+            className="text-blue-500 underline hover:text-blue-700"
+            href="/quiz"
+          >
+            Take the quiz here!
+          </Link>
+        </p>
+      </div>
+      <div className="py-5">
+        <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+          Quiz Resources
+        </h2>
+        <p className="text-gray-600">
+          Your specific quiz resources will be displayed here when your results
+          are available.
+        </p>
+      </div>
+    </div>
+  )
 }
 export default ProfileQuizResults
